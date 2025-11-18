2674eda71893b571b1357e967ac083c0e750453d
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-11-18 17:48:27 +0100

    Rebuild rapidjson with fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,11 @@
 mkdir build
 cd build
 
-cmake -DRAPIDJSON_HAS_STDSTRING=ON \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -G "Ninja" ${CMAKE_ARGS} \
+      -DRAPIDJSON_HAS_STDSTRING=ON \
       -DRAPIDJSON_BUILD_TESTS=OFF \
       -DRAPIDJSON_BUILD_EXAMPLES=OFF \
       -DRAPIDJSON_BUILD_DOC=OFF \
-      -DCMAKE_VERBOSE_MAKEFILE=ON \
-      -DCMAKE_BUILD_TYPE=release \
       ..
 
-make install
+cmake --build . --target install -j${CPU_COUNT}
